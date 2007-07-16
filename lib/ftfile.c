3ffb185f30ad502eb57f0721774f4ceda1b677c8
Denis Shaposhnikov <dsh@vlink.ru>
2007-07-16 06:10:24 +0100

    Fix crash on expiring old files.
    Signed-off-by: Paul P. Komkoff Jr <i@stingr.net>

--- lib/ftfile.c
+++ lib/ftfile.c
@@ -311,7 +311,7 @@ int ftfile_add_tail(struct ftfile_entries *fte, char *fname, off_t size,
 int ftfile_expire (struct ftfile_entries *fte, int doit, int curbytes)
 {
   u_int i;
-  struct ftfile_entry *n1;
+  struct ftfile_entry *n1, *n2;
   u_int64 bytes;
 
   /*
@@ -323,15 +323,20 @@ int ftfile_expire (struct ftfile_entries *fte, int doit, int curbytes)
   bytes = 0;
 
   if (fte->max_files && (fte->num_files > fte->max_files)) {
+    n2 = NULL;
     FT_TAILQ_FOREACH(n1, &fte->head, chain) {
+      if (n2 != NULL) {
+	ftfile_entry_free(n2);
+	n2 = NULL;
+      }
       fterr_info("remove/1 %s", n1->name);
       bytes += n1->size;
       ++i;
       if (doit) {
+        n2 = n1;
         FT_TAILQ_REMOVE(&fte->head, n1, chain);
         if (unlink(n1->name) == -1) 
           fterr_warn("unlink(%s)", n1->name);
-        ftfile_entry_free(n1);
       } /* doit */
       if ((fte->num_files - i) <= fte->max_files)
         break;
@@ -340,6 +345,10 @@ int ftfile_expire (struct ftfile_entries *fte, int doit, int curbytes)
       fte->num_files -= i;
       fte->num_bytes -= bytes;
     } /* doit */
+    if (n2 != NULL) {
+      ftfile_entry_free(n2);
+      n2 = NULL;
+    }
   } /* if */
 
   if (debug)
@@ -354,15 +363,20 @@ int ftfile_expire (struct ftfile_entries *fte, int doit, int curbytes)
    */
 
   if (fte->max_bytes && (fte->num_bytes+curbytes > fte->max_bytes)) {
+    n2 = NULL;
     FT_TAILQ_FOREACH(n1, &fte->head, chain) {
+      if (n2 != NULL) {
+	ftfile_entry_free(n2);
+	n2 = NULL;
+      }
       fterr_info("remove/2 %s", n1->name);
       bytes += n1->size;
       ++i;
       if (doit) {
+        n2 = n1;
         FT_TAILQ_REMOVE(&fte->head, n1, chain);
         if (unlink(n1->name) == -1) 
           fterr_warn("unlink(%s)", n1->name);
-        ftfile_entry_free(n1);
       } /* doit */
       if ((fte->num_bytes+curbytes - bytes) <= fte->max_bytes)
         break;
@@ -371,6 +385,10 @@ int ftfile_expire (struct ftfile_entries *fte, int doit, int curbytes)
       fte->num_files -= i;
       fte->num_bytes -= bytes;
     } /* doit */
+    if (n2 != NULL) {
+      ftfile_entry_free(n2);
+      n2 = NULL;
+    }
   } /* if */
 
   if (debug)
@@ -762,13 +780,19 @@ void ftfile_free(struct ftfile_entries *fte)
 {
   struct ftfile_entry *n1, *n2;
 
+  n2 = NULL;
   FT_TAILQ_FOREACH(n1, &fte->head, chain) {
+    if (n2 != NULL) {
+      ftfile_entry_free(n2);
+      n2 = NULL;
+    }
     FT_TAILQ_REMOVE(&fte->head, n1, chain);
     n2 = n1;
-    n1 = FT_TAILQ_NEXT(n1, chain);
+  }
+
+  if (n2 != NULL) {
     ftfile_entry_free(n2);
-    if (!n1)
-      break;
+    n2 = NULL;
   }
 
 } /* ftfile_free */
