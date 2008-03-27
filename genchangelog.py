70268773a6ea5d7ecab17dcbe6a734ead00a0aa1
Paul P. Komkoff Jr <i@stingr.net>
2008-03-27 10:31:03 +0000

    New changelog builder

---
+++ genchangelog.py
@@ -0,0 +1,33 @@
+#!/bin/env python
+
+import subprocess
+
+tags = map(lambda x: x.strip(), subprocess.Popen(['git', 'tag', '-l'], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0].splitlines())
+
+upper = len(tags) - 1
+
+for i in xrange(upper, -1, -1):
+  if i == upper:
+    first = tags[i]
+    last = ''
+    m = 'AFTER %s' % tags[i]
+  elif i == -1:
+    first = ''
+    last = tags[i+1]
+    m = 'IN %s' % tags[i+1]
+  else:
+    first = tags[i]
+    last = tags[i+1]
+    m = 'IN %s' % tags[i+1]
+
+  r = '%s..%s' % (first, last)
+
+  clog = subprocess.Popen(['git', 'shortlog', r], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
+
+  if not clog.strip():
+    continue
+
+  print "          CHANGES %s:" % m
+  print
+  print clog
+
